a36532004b3d65d172c2ca01566db77435ca839b
rs <salakhiev.ruslan@gmail.com>
2022-03-26 23:37:03 +0300

    new keyboards and buttons

--- src/keyboards/keyboards.py
+++ src/keyboards/keyboards.py
@@ -7,12 +7,24 @@ from ..models.item import Item
 from ..models.legal_entity import LegalEntity
 
 
-def approve_buttons(text: str) -> List[List[InlineKeyboardButton]]:
+def return_to_main_menu_button(text: str = 'В меню') -> List[InlineKeyboardButton]:
     return [
-        [
-            InlineKeyboardButton(text=text,
-                                 callback_data='approved'),
-        ],
+        InlineKeyboardButton(text=text,
+                             callback_data='main_menu'),
+    ]
+
+
+def approve_button(text: str) -> List[InlineKeyboardButton]:
+    return [
+        InlineKeyboardButton(text=text,
+                             callback_data='approved'),
+    ]
+
+
+def edit_button(text: str, previous_step: str) -> List[InlineKeyboardButton]:
+    return [
+        InlineKeyboardButton(text=text,
+                             callback_data='return_to_' + previous_step),
     ]
 
 
@@ -27,15 +39,28 @@ def main_menu_keyboard() -> InlineKeyboardMarkup:
                                  callback_data='act'),
         ],
     ]
+
     return InlineKeyboardMarkup(buttons)
 
 
-def confirm_legal_entity_keyboard() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(approve_buttons(text='Все верно'))
+def confirm_legal_entity_and_get_invoice_keyboard() -> InlineKeyboardMarkup:
+    buttons = [
+        approve_button(text='Все верно, выставляем счет!'),
+        edit_button(text='Изменить юр.лицо',
+                    previous_step='legal_entity'),
+    ]
+    return InlineKeyboardMarkup(buttons)
 
 
-def final_confirm_keyboard() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(approve_buttons(text='Все верно, выставляем счет'))
+def confirm_items_keyboard(show_approve: bool) -> InlineKeyboardMarkup:
+    buttons = []
+    if show_approve:
+        buttons.append(approve_button(text='Все верно'))
+
+    buttons.append(edit_button(text='Поменять кол-во',
+                               previous_step='select_item'))
+
+    return InlineKeyboardMarkup(buttons)
 
 
 def select_by_kpp_keyboard(entities: list[LegalEntity]) -> InlineKeyboardMarkup:
@@ -52,6 +77,17 @@ def select_by_kpp_keyboard(entities: list[LegalEntity]) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(buttons)
 
 
+def select_course_keyboard() -> InlineKeyboardMarkup:
+    buttons = [
+        [
+            InlineKeyboardButton(text='Асинхронная архитектура',  # replace hardcode
+                                 callback_data='course_aa'),
+        ],
+    ]
+
+    return InlineKeyboardMarkup(buttons)
+
+
 def select_item_keyboard(items: list[Item]) -> InlineKeyboardMarkup:
     buttons = []
 
@@ -59,7 +95,7 @@ def select_item_keyboard(items: list[Item]) -> InlineKeyboardMarkup:
         buttons.append(
             [
                 InlineKeyboardButton(text=f'{item.user_name}, цена: {item.price}р.',
-                                     callback_data=str(item.user_name)),
+                                     callback_data='item_noaction'),
             ],
         )
         buttons.append(
@@ -67,7 +103,7 @@ def select_item_keyboard(items: list[Item]) -> InlineKeyboardMarkup:
                 InlineKeyboardButton(text='-',
                                      callback_data='item_minus_' + item.user_name),
                 InlineKeyboardButton(text=f'{item.amount} шт.',
-                                     callback_data='...'),
+                                     callback_data='item_noaction'),
                 InlineKeyboardButton(text='+',
                                      callback_data='item_plus_' + item.user_name),
             ],
@@ -79,4 +115,17 @@ def select_item_keyboard(items: list[Item]) -> InlineKeyboardMarkup:
                                  callback_data='item_finish'),
         ],
     )
+
+    return InlineKeyboardMarkup(buttons)
+
+
+def link_to_invoice_keyboard(url: str) -> InlineKeyboardMarkup:
+    buttons = [
+        [
+            InlineKeyboardButton(text='Скачать счет',
+                                 url=url),
+        ],
+        return_to_main_menu_button(),
+    ]
+
     return InlineKeyboardMarkup(buttons)
